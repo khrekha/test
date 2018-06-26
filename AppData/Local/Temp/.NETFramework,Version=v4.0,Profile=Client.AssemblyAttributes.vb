5774c2eb2a6ef59236bb42380197b0af691af357
khrekha <rekhadoeacc@gmail.com>
2018-06-26 16:27:17 +0530

    initial

---
+++ AppData/Local/Temp/.NETFramework,Version=v4.0,Profile=Client.AssemblyAttributes.vb
@@ -0,0 +1,6 @@
+Option Strict Off
+            Option Explicit On
+
+            Imports System
+            Imports System.Reflection
+            <Assembly: Global.System.Runtime.Versioning.TargetFrameworkAttribute(".NETFramework,Version=v4.0,Profile=Client", FrameworkDisplayName:=".NET Framework 4 Client Profile")>
